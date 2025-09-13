6cede6f08c11b168e84c24ee572c64008eb91423
Xavier <xav@arch.localdomain>
2025-09-13 14:56:29 +0200

    New mini plugins

--- lua/plugins/plugin.lua
+++ lua/plugins/plugin.lua
@@ -1,10 +1,12 @@
 return {
 	"folke/which-key.nvim",
 	{ "folke/neoconf.nvim", cmd = "Neoconf" },
-	{ "echasnovski/mini.icons", version = "*", opts = {} },
-	{ "echasnovski/mini.diff", version = "*" , opts = {} },
-	{ "echasnovski/mini-git", version = "*", main = "mini.git" },
-	{ "echasnovski/mini.statusline", version = "*", opts = {} },
-	{ "echasnovski/mini.pick", version = "*", opts = {} },
+	{ "nvim-mini/mini.icons", version = "*", opts = {} },
+	{ "nvim-mini/mini.diff", version = "*" , opts = {} },
+	{ "nvim-mini/mini-git", version = "*", main = "mini.git", opts = {} },
+	{ "nvim-mini/mini.statusline", version = "*", opts = {} },
+	{ "nvim-mini/mini.pick", version = "*", opts = {} },
 	{ 'nvim-mini/mini.pairs', version = '*', opts = {} },
+	{ 'nvim-mini/mini.snippets', version = '*', opts = {} },
+	{ 'nvim-mini/mini.completion', version = '*', opts = {} },
 }
